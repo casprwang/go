bfca394fee7f6c19b268dbbbf23318828d1cca2e
wangsongiam <wangsongiam@gmail.com>
2017-09-10 01:53:06 -0400

    update

--- for.go
+++ for.go
@@ -4,9 +4,8 @@ import "fmt"
 
 func main() {
 
-  i:= 1
-  fmt.Println(i)
-  i = i + 1
+	i := 1
+	fmt.Println(i)
+	i = i + 1
 
 }
-
--- values.go
+++ values.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("go" + "lang")
-  fmt.Println("7/3", 9/3)
-  fmt.Println(true && false)
+	fmt.Println("go" + "lang")
+	fmt.Println("%d7/3", 9/3)
+	fmt.Println(true && false)
 }
